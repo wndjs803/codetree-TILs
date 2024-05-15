c986fb5bf9cb03c23b65a0f2687450ee126ff496
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2024-05-15 19:22:36 +0900

    Title: 최고의 33위치, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240515/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,19 @@
+n =  int(input())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+grid = 3
+max_cnt = 0
+for i in range(n):
+    if i+3 > n: break
+    for j in range(n):
+        if j+3 > n: continue
+        cnt = 0
+        for k in range(i, i+3):
+            for l in range(j, j+3):
+                if arr[k][l] == 1:
+                    cnt += 1
+        
+        max_cnt = max(max_cnt, cnt)
+
+print(max_cnt)
