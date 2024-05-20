dd9ee2bb83d5f7f5c7f9fb63e2a05359acfcd733
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2024-05-20 13:58:31 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 414ms, Memory: 29MB, Status: Passed - CodeTree

--- 240520/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 240520/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -14,7 +14,7 @@ def recur(index):
         return
     
     for i in range(1, k+1):
-        if i > 2 and (arr[i-2] == i and arr[i-1] == i):
+        if index > 1 and (arr[index-2] == i and arr[index-1] == i):
             continue 
         arr.append(i)
         recur(index+1)
