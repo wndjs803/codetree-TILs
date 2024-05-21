47d723f442f12d0fd406989ffa0bb874f32cb062
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2024-05-21 20:26:46 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240521/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240521/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -16,15 +16,17 @@ def dfs(x, y):
     if x == n -1 and y == m - 1:
         check = 1
         return
-    
+        
     for i in range(2):
         nx = x + dx[i]
         ny = y + dy[i]
         
-        if nx >= 0 and nx < 5 and ny >= 0 and ny < m:
-            if visited[nx][ny] == False and graph[x][y] == 1:
+        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+            if visited[nx][ny] == False and graph[nx][ny] == 1:
                 x, y = nx, ny
                 visited[x][y] = True
                 dfs(x, y)
+
+dfs(0, 0)
 
 print(check)
