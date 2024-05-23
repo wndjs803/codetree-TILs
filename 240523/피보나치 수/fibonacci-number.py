2e7b76e4e278a7b0e2b0f0601decc8ad75495f47
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2024-05-23 19:41:55 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240523/피보나치 수/fibonacci-number.py
+++ 240523/피보나치 수/fibonacci-number.py
@@ -2,9 +2,19 @@ n = int(input())
 
 dp = [0] * (n + 1)
 
-dp[1] = 1
+# dp[1] = 1
 
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+# for i in range(2, n+1):
+#     dp[i] = dp[i-1] + dp[i-2]
 
+def fibbo(n):
+    if dp[n] != 0:
+        return dp[n]
+    
+    if n <= 2:
+        dp[n] = 1
+    else:
+        dp[n] = fibbo[n -1] + fibbo[n-2]
+
+fibbo(n)
 print(dp[n])
