edd43b965b52f23695b97ed5f2334c5dd5ee3de9
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2024-05-23 21:19:56 +0900

    Title: 최대 증가 부분 수열, Time: 114ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240523/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240523/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -12,7 +12,7 @@ for i in range(1, n):
         if dp[j] == -1:
             continue
         
-        if arr[j] <= arr[i]:
+        if arr[j] < arr[i]:
             dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
